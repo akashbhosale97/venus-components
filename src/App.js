79e6999f383b89fc3b4a2e8c4b4d9e43cee64353
Akash Bhosale <MY_NAME@example.com>
2022-05-05 12:59:53 +0530

    modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { cbModal, Icon } from "@contentstack/venus-components";
+import expand from "./assets/Icon.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const ModalComponent = ({ closeModal }) => (
+    <div
+      style={{
+        width: "calc(100vw - 100px)",
+        height: "calc(100vh - 100px)",
+        borderRadius: "inherit",
+      }}
+    >
+      <div className="flex FullPage_Modal_Header">
+        <h6 className="ml-30 mt-20"> Header </h6>
+        <Icon
+          icon="Compress"
+          size="small"
+          className="Tab__icon mt-20"
+          hover={true}
+          hoverType="secondary"
+          style={{ marginRight: "30px", marginLeft: "auto", cursor: "pointer" }}
+          onClick={closeModal}
+        />
+      </div>
+      <div className="mt-30 mr-20 ml-20 mb-20">Modal content goes here</div>
     </div>
+  );
+
+  const openModal = () => {
+    cbModal({
+      component: (props) => <ModalComponent {...props} />,
+      modalProps: {
+        size: "customSize",
+        shouldCloseOnOverlayClick: true,
+      },
+    });
+  };
+
+  return (
+    <>
+      <div className="d-flex" onClick={openModal} role="button">
+        <img src={expand} alt="modal" className="me-2" />
+        click me
+      </div>
+    </>
   );
 }
 
